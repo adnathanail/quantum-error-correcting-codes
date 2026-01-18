45e68685627a37d14f916a78dd96db3af84c354c
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-01-19 00:41:26 +0100

    Export all Shor's functions in __init__.py

--- qecc/__init__.py
+++ qecc/__init__.py
@@ -1,4 +1,12 @@
-from .nine_qubit_shors_code import get_nine_qubit_shors_code_encoding_circuit
+from .nine_qubit_shors_code import (
+    apply_nine_qubit_shors_code_bit_flip_correction,
+    apply_nine_qubit_shors_code_phase_flip_correction,
+    get_nine_qubit_shors_code_bit_flip_syndrome_extraction_circuit,
+    get_nine_qubit_shors_code_decoding_circuit,
+    get_nine_qubit_shors_code_encoding_circuit,
+    get_nine_qubit_shors_code_phase_flip_syndrome_extraction_circuit,
+    get_nine_qubit_shors_code_syndrome_extraction_circuit,
+)
 from .three_qubit_bit_flip import apply_three_qubit_bit_flip_correction, get_three_qubit_bit_flip_encoding_decoding_circuit, get_three_qubit_bit_flip_syndrome_extraction_circuit
 from .three_qubit_phase_flip import (
     apply_three_qubit_phase_flip_correction,
@@ -8,9 +16,15 @@ from .three_qubit_phase_flip import (
 )
 
 __all__ = [
+    "apply_nine_qubit_shors_code_bit_flip_correction",
+    "apply_nine_qubit_shors_code_phase_flip_correction",
     "apply_three_qubit_bit_flip_correction",
     "apply_three_qubit_phase_flip_correction",
+    "get_nine_qubit_shors_code_bit_flip_syndrome_extraction_circuit",
+    "get_nine_qubit_shors_code_decoding_circuit",
     "get_nine_qubit_shors_code_encoding_circuit",
+    "get_nine_qubit_shors_code_phase_flip_syndrome_extraction_circuit",
+    "get_nine_qubit_shors_code_syndrome_extraction_circuit",
     "get_three_qubit_bit_flip_encoding_decoding_circuit",
     "get_three_qubit_bit_flip_syndrome_extraction_circuit",
     "get_three_qubit_phase_flip_decoding_circuit",
