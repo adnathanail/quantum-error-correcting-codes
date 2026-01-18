bb8ef153371e3d14393b977275a049c60b08b4f7
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-01-18 18:10:00 +0100

    Generate images of 3 qubit bit flip encoding at all stages

--- scripts/generate_circuits.py
+++ scripts/generate_circuits.py
@@ -1,8 +1,33 @@
 from pathlib import Path
 
-from qecc.three_qubit_bit_flip import get_three_qubit_bit_flip_encoding_decoding_circuit
+from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
+
+from qecc.three_qubit_bit_flip import apply_three_qubit_syndrome_correction, get_three_qubit_bit_flip_encoding_decoding_circuit, get_three_qubit_bit_flip_syndrome_extraction_circuit
 
 out_dir = Path(__file__).parent.parent / "imgs"
 
-circuit = get_three_qubit_bit_flip_encoding_decoding_circuit()
-circuit.draw("mpl", filename=str(out_dir / "three_qubit_bit_flip_encoding_decoding.png"))
+
+def draw_circuit(circuit: QuantumCircuit, file_name: str) -> None:
+    circuit.draw("mpl", filename=str(out_dir / file_name))
+
+
+# === 3 qubit bit-flip ===
+# Encoding
+draw_circuit(get_three_qubit_bit_flip_encoding_decoding_circuit(), "three_qubit_bit_flip_encoding.png")
+
+# Syndrome extraction
+qc = QuantumCircuit(5)
+qc.compose(get_three_qubit_bit_flip_encoding_decoding_circuit(), qubits=(0, 1, 2), inplace=True)
+qc.barrier()
+qc.compose(get_three_qubit_bit_flip_syndrome_extraction_circuit(), qubits=(0, 1, 2, 3, 4), inplace=True)
+draw_circuit(qc, "three_qubit_bit_flip_syndrome_extraction.png")
+
+# Error correction
+qureg, clreg = QuantumRegister(5), ClassicalRegister(2)
+qc = QuantumCircuit(qureg, clreg)
+qc.compose(get_three_qubit_bit_flip_encoding_decoding_circuit(), qubits=(0, 1, 2), inplace=True)
+qc.barrier()
+qc.compose(get_three_qubit_bit_flip_syndrome_extraction_circuit(), qubits=(0, 1, 2, 3, 4), inplace=True)
+qc.barrier()
+apply_three_qubit_syndrome_correction(qc, clreg)
+draw_circuit(qc, "three_qubit_bit_flip_error_correction.png")
