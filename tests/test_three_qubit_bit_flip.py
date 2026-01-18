567b2fcecec3656cef563d4e0e54bb0535542eaa
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-01-18 16:44:15 +0100

    Refactor 3 qubit bit flip encoding/decoding tests

--- tests/test_three_qubit_bit_flip.py
+++ tests/test_three_qubit_bit_flip.py
@@ -1,4 +1,5 @@
 from qiskit import QuantumCircuit
+from qiskit.quantum_info import Statevector
 
 from qecc import (
     get_three_qubit_bit_flip_encoding_decoding_circuit,
@@ -10,93 +11,75 @@ from .utils import CompBasisState, QuantumCircuitTest
 
 
 class TestThreeQubitBitFlipEncodingDecoding(QuantumCircuitTest):
-    def test_encoding_0(self):
-        qc = QuantumCircuit(3)
-        qc.initialize(CompBasisState.ZERO, [0])
-        # Encode
+    @staticmethod
+    def get_initialized_qc(state_to_initialize: Statevector) -> QuantumCircuit:
+        """
+        Given a (normalized) state vector, return a 3-qubit quantum circuit with the first qubit initialised to the input vector
+        """
+        out = QuantumCircuit(3)
+        out.initialize(state_to_initialize, [0])
+        return out
+
+    @staticmethod
+    def encode_or_decode(qc: QuantumCircuit) -> None:
+        """
+        Given a 3-qubit quantum circuit, apply the 3 qubit bit flip encoding/decoding circuit
+        """
         qc.compose(
             get_three_qubit_bit_flip_encoding_decoding_circuit(),
             qubits=(0, 1, 2),
             inplace=True,
         )
-        # Measure
-        qc.measure_all()
 
-        assert self.simulate_circuit(qc) == {"000": 1024}
+    @classmethod
+    def check_results_one_result(cls, qc: QuantumCircuit, correct_result: str) -> None:
+        """
+        Given a quantum circuit and a single correct result, measure the circuit, and check the results of the measurement match the inputted correct result
+        """
+        qc.measure_all()
+        # Reverse the order of correct results, because Qiskit works backwards
+        correct_result_little_endian = correct_result[::-1]
+        assert cls.simulate_circuit(qc) == {correct_result_little_endian: 1024}
+
+    @classmethod
+    def check_results_two_results_50_50(cls, qc: QuantumCircuit, correct_results: list[str]) -> None:
+        """
+        Given a quantum circuit and a list of 2 correct results, measure the circuit, and check the results of the measurement are approximately 50/50 between the two correct results (within +- 15%)
+        """
+        qc.measure_all()
+        measurements = cls.simulate_circuit(qc)
+        # Reverse the order of correct results, because Qiskit works backwards
+        correct_results_little_endian = [res[::-1] for res in correct_results]
+        # Test we have roughly 50/50 (+- 15%) of each correct result
+        assert set(measurements.keys()) == set(correct_results_little_endian)
+        assert 0.85 <= measurements[correct_results_little_endian[0]] / measurements[correct_results_little_endian[1]] <= 1.15
+
+    def test_encoding_0(self):
+        qc = self.get_initialized_qc(CompBasisState.ZERO)
+        self.encode_or_decode(qc)
+        self.check_results_one_result(qc, "000")
 
     def test_encoding_1(self):
-        qc = QuantumCircuit(3)
-        qc.initialize(CompBasisState.ONE, [0])
-        # Encode
-        qc.compose(
-            get_three_qubit_bit_flip_encoding_decoding_circuit(),
-            qubits=(0, 1, 2),
-            inplace=True,
-        )
-        # Measure
-        qc.measure_all()
-
-        assert self.simulate_circuit(qc) == {"111": 1024}
+        qc = self.get_initialized_qc(CompBasisState.ONE)
+        self.encode_or_decode(qc)
+        self.check_results_one_result(qc, "111")
 
     def test_encoding_decoding_1(self):
-        qc = QuantumCircuit(3)
-        qc.initialize(CompBasisState.ONE, [0])
-        # Encode
-        qc.compose(
-            get_three_qubit_bit_flip_encoding_decoding_circuit(),
-            qubits=(0, 1, 2),
-            inplace=True,
-        )
-        # Decode
-        qc.compose(
-            get_three_qubit_bit_flip_encoding_decoding_circuit(),
-            qubits=(0, 1, 2),
-            inplace=True,
-        )
-        # Measure
-        qc.measure_all()
-
-        assert self.simulate_circuit(qc) == {"001": 1024}
+        qc = self.get_initialized_qc(CompBasisState.ONE)
+        self.encode_or_decode(qc)
+        self.encode_or_decode(qc)
+        self.check_results_one_result(qc, "100")
 
     def test_encoding_plus(self):
-        qc = QuantumCircuit(3)
-        qc.initialize(HadBasisState.PLUS, [0])
-        # Encode
-        qc.compose(
-            get_three_qubit_bit_flip_encoding_decoding_circuit(),
-            qubits=(0, 1, 2),
-            inplace=True,
-        )
-        # Measure
-        qc.measure_all()
-
-        measurements = self.simulate_circuit(qc)
-        # Test we have roughly 50/50 (+- 10%) 000 and 111 state
-        assert set(measurements.keys()) == {"000", "111"}
-        assert 0.9 <= measurements["000"] / measurements["111"] <= 1.1
+        qc = self.get_initialized_qc(HadBasisState.PLUS)
+        self.encode_or_decode(qc)
+        self.check_results_two_results_50_50(qc, ["000", "111"])
 
     def test_encoding_decoding_plus(self):
-        qc = QuantumCircuit(3)
-        qc.initialize(HadBasisState.PLUS, [0])
-        # Encode
-        qc.compose(
-            get_three_qubit_bit_flip_encoding_decoding_circuit(),
-            qubits=(0, 1, 2),
-            inplace=True,
-        )
-        # Decode
-        qc.compose(
-            get_three_qubit_bit_flip_encoding_decoding_circuit(),
-            qubits=(0, 1, 2),
-            inplace=True,
-        )
-        # Measure
-        qc.measure_all()
-
-        measurements = self.simulate_circuit(qc)
-        # Test we have roughly 50/50 (+- 15%) 000 and 001 state
-        assert set(measurements.keys()) == {"000", "001"}
-        assert 0.85 <= measurements["000"] / measurements["001"] <= 1.15
+        qc = self.get_initialized_qc(HadBasisState.PLUS)
+        self.encode_or_decode(qc)
+        self.encode_or_decode(qc)
+        self.check_results_two_results_50_50(qc, ["000", "100"])
 
 
 class TestThreeQubitBitFlipSyndromeExtraction(QuantumCircuitTest):
