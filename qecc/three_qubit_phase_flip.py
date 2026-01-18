3f4a31155f5c3180637f2d9bc71e2f73581eac19
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-01-18 19:06:07 +0100

    3 qubit phase flip syndrom extraction

--- qecc/three_qubit_phase_flip.py
+++ qecc/three_qubit_phase_flip.py
@@ -1,3 +1,7 @@
+"""
+Reference https://quantum.cloud.ibm.com/learning/en/courses/foundations-of-quantum-error-correction/correcting-quantum-errors/repetition-codes#phase-flip-errors
+"""
+
 from qiskit import QuantumCircuit
 
 
@@ -21,4 +25,32 @@ def get_three_qubit_phase_flip_decoding_circuit() -> QuantumCircuit:
     out.h(2)
     out.cx(0, 1)
     out.cx(0, 2)
+    return out
+
+
+def get_three_qubit_phase_flip_syndrome_extraction_circuit() -> QuantumCircuit:
+    """
+    Error in qubit 0 gives syndrome 01, qubit 1 10, qubit 2 11
+    """
+    out = QuantumCircuit(5)
+    out.h(3)
+    out.h(4)
+    out.cx(
+        3,
+        0,
+    )
+    out.cx(
+        4,
+        1,
+    )
+    out.cx(
+        3,
+        2,
+    )
+    out.cx(
+        4,
+        2,
+    )
+    out.h(3)
+    out.h(4)
     return out
--- tests/test_three_qubit_phase_flip.py
+++ tests/test_three_qubit_phase_flip.py
@@ -1,12 +1,14 @@
 from qiskit import QuantumCircuit
 
 from qecc import get_three_qubit_phase_flip_encoding_circuit
-from qecc.three_qubit_phase_flip import get_three_qubit_phase_flip_decoding_circuit
+from qecc.three_qubit_phase_flip import get_three_qubit_phase_flip_decoding_circuit, get_three_qubit_phase_flip_syndrome_extraction_circuit
 
 from .utils import CompBasisState, HadBasisState, QuantumCircuitTest
 
 
 class TestThreeQubitPhaseFlipEncodingDecoding(QuantumCircuitTest):
+    ALL_THREE_QUBIT_STATES: tuple[str, ...] = ("000", "001", "010", "011", "100", "101", "110", "111")
+
     @staticmethod
     def encode(qc: QuantumCircuit) -> None:
         """
@@ -24,13 +26,13 @@ class TestThreeQubitPhaseFlipEncodingDecoding(QuantumCircuitTest):
     def test_encoding_0(self):
         qc, _, _ = self.get_initialized_qc(CompBasisState.ZERO)
         self.encode(qc)
-        self.check_results_n_results_even_chance(qc, ("000", "001", "010", "011", "100", "101", "110", "111"))
+        self.check_results_n_results_even_chance(qc, self.ALL_THREE_QUBIT_STATES)
 
     def test_encoding_1(self):
         qc, _, _ = self.get_initialized_qc(CompBasisState.ONE)
         self.encode(qc)
         # Same as for 0, because the phase on |111> vs -|111> isn't detectable by measurement
-        self.check_results_n_results_even_chance(qc, ("000", "001", "010", "011", "100", "101", "110", "111"))
+        self.check_results_n_results_even_chance(qc, self.ALL_THREE_QUBIT_STATES)
 
     def test_encoding_plus(self):
         qc, _, _ = self.get_initialized_qc(HadBasisState.PLUS)
@@ -55,3 +57,22 @@ class TestThreeQubitPhaseFlipEncodingDecoding(QuantumCircuitTest):
         self.encode(qc)
         self.decode(qc)
         self.check_results_two_results_50_50(qc, ("000", "001"))
+
+
+class TestThreeQubitPhaseFlipSyndromeExtraction(TestThreeQubitPhaseFlipEncodingDecoding):
+    @staticmethod
+    def syndrome_extraction(qc: QuantumCircuit) -> None:
+        """
+        Given a 5-qubit quantum circuit, apply the 3 qubit phase flip syndrome extraction circuit
+        """
+        qc.compose(
+            get_three_qubit_phase_flip_syndrome_extraction_circuit(),
+            qubits=(0, 1, 2, 3, 4),
+            inplace=True,
+        )
+
+    def test_encoding_0_syndrome_no_error(self):
+        qc, _, _ = self.get_initialized_qc(CompBasisState.ZERO, num_qubits=5)
+        self.encode(qc)
+        self.syndrome_extraction(qc)
+        self.check_results_n_results_even_chance(qc, tuple("00" + state for state in self.ALL_THREE_QUBIT_STATES))
